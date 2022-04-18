c62fe5587bc07eaa0fbf047a27595f86cb7eeb40
wprzadka <wiktor.przadka.1@gmail.com>
2022-04-18 21:29:48 +0200

    reading from file

--- main.cpp
+++ main.cpp
@@ -3,13 +3,26 @@
 //
 
 #include <string>
+#include <fstream>
 #include "rans.h"
 
 
 int main(){
-    RANS rans{};
 
-    std::string message = "AABAA4CCAAD12";
+    std::ifstream file_reader("example.txt", std::ios::binary);
+    if(!file_reader.is_open()){
+        printf("File can't be opened\n");
+        return 1;
+    }
+
+    uint8_t size = 16;
+    char* mem_block = new char [size];
+    file_reader.read(mem_block, size);
+    printf("input = %s\n", mem_block);
+
+    std::string message = mem_block;
+
+    RANS rans{};
     rans.init(message);
 
     uint64_t enc = rans.encode(message);
@@ -17,4 +30,6 @@ int main(){
 
     std::string dec = rans.decode(enc);
     printf("decoded\n%s\n", dec.c_str());
+
+    delete[] mem_block;
 }
