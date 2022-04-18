cdf30c16caf43f7fee84905156f284e2cbe1fd8e
wprzadka <wiktor.przadka.1@gmail.com>
2022-04-19 00:12:05 +0200

    add command line interface

--- main.cpp
+++ main.cpp
@@ -5,20 +5,17 @@
 #include <string>
 #include <fstream>
 #include "rans.h"
+#include <getopt.h>
 
+int encode_file(char* file_name){
 
-int main(){
-
-    std::ifstream file_reader("example.txt", std::ios::binary);
+    std::ifstream file_reader(file_name, std::ios::binary);
     if(!file_reader.is_open()){
-        printf("File can't be opened\n");
-        return 1;
+        printf("file \"%s\" can't be opened.\n", file_name);
     }
-
     uint8_t size = 16;
     char* mem_block = new char [size];
     file_reader.read(mem_block, size);
-    printf("input = %s\n", mem_block);
 
     std::string message = mem_block;
 
@@ -27,9 +24,49 @@ int main(){
 
     uint64_t enc = rans.encode(message);
     printf("encoded\n%lu\n", enc);
+    return 0;
+}
 
-    std::string dec = rans.decode(enc);
-    printf("decoded\n%s\n", dec.c_str());
+int decode_file(char* file_name){
 
-    delete[] mem_block;
+//    std::string dec = rans.decode(enc);
+//    printf("decoded\n%s\n", dec.c_str());
+    printf("not implemented\n");
+    return 0;
+}
+
+int main(int argc, char** argv){
+
+    option option_names[] = {
+            {"version", no_argument, nullptr, 'v'},
+            {"help", no_argument, nullptr, 'h'},
+            {"encode", required_argument, nullptr, 'e'},
+            {"decode", required_argument, nullptr, 'd'}
+    };
+
+    int opt;
+    opt = getopt_long(argc, argv, "vh:e:d", option_names, nullptr);
+    switch (opt) {
+        case 'v':
+            printf("0.1");
+            return 0;
+        case 'h':
+            printf("possible operations:\n"
+                   "-v --version\n"
+                   "-h --help\n"
+                   "-e --encode file_name\n"
+                   "-d --decode file_name\n");
+            return 0;
+        case 'e':
+            return encode_file(optarg);
+        case 'd':
+            return decode_file(optarg);
+        case ':':
+            printf("Option requires an argument.\n");
+            return 1;
+        case '?':
+        default:
+            printf("Unknown argument \"%c\" provided.\n", optopt);
+            return 1;
+    }
 }
