02054bf15799f9c6fe497e8cc012884b2caa78c7
wprzadka <wiktor.przadka.1@gmail.com>
2022-04-29 14:16:57 +0200

    Implement test for encoding data

--- tests/rans_tests.cpp
+++ tests/rans_tests.cpp
@@ -2,12 +2,46 @@
 // Created by viking on 26.04.22.
 //
 
+#include <fstream>
 #include "gtest/gtest.h"
 #include "../rans.h"
 
-class RANS_Test: public ::testing::Test{
-
+class RANS_Coding_Test: public ::testing::Test{
+public:
+    const uint16_t plain_text_size = 1080;
+    const char* plain_text =
+            "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin.\n"
+            "He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections.\n"
+            "The bedding was hardly able to cover it and seemed ready to slide off any moment.\n"
+            "His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.\n"
+            "\"What's happened to me? \" he thought. It wasn't a dream.\n"
+            "His room, a proper human room although a little too small, lay peacefully between its four familiar walls.\n"
+            "A collection of textile samples lay spread out on the table - Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out of an illustrated magazine and housed in a nice, gilded frame.\n"
+            "It showed a lady fitted out with a fur hat and fur boa who sat upright, raising a heavy fur muff that covered the whole of her lower arm towards the viewer.\n"
+            "Gregor then turned to look out the window at the dull weather. Drops";
+    const uint16_t encoded_text_size = 588;
 };
+
+TEST_F(RANS_Coding_Test, encode){
+    RANS rans{};
+    rans.prepare_frequencies(plain_text, plain_text_size);
+
+    std::string encoded = rans.encode(plain_text, plain_text_size);
+    ASSERT_EQ(encoded_text_size, encoded.size());
+
+    std::ifstream exact("encoded.bin", std::ios::binary | std::ios::in);
+    if (!exact){
+        printf("Can't open file\n");
+        FAIL();
+    }
+    char* encoded_text = new char[encoded_text_size];
+    exact.read(encoded_text, encoded_text_size);
+
+    for (int i = 0; i < encoded_text_size; ++i){
+        ASSERT_EQ(encoded_text[i], encoded[i]);
+    }
+    delete[] encoded_text;
+}
 
 TEST(RANS_Test, prepare_frequencies){
     RANS rans{};
